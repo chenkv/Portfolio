541eca2a2129017ad8c0eadb4c2a70db37aa9ae3
chenkv <chen.kevin885@gmail.com>
2023-01-12 11:43:44 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `My Porfolio Website`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
